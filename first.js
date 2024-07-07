44ae793fbafbb58d6807accd7fa1eb0a43126354
Samip Nepal <samipnepal5@gmail.com>
2024-07-07 17:38:07 +1000

    Selection with Tags

--- first.js
+++ first.js
@@ -10,4 +10,6 @@ console.dir(heading);
 let headings=document.getElementsByClassName("class")
 console.log(headings);//for print of html 
 console.dir(headings);
-
+let pra=document.getElementsByTagName("p")
+console.log(pra);//for print of html 
+console.dir(pra);
