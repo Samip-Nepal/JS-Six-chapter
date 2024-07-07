3074983ebbc78b38dc1343038c68789b622659ec
Samip Nepal <samipnepal5@gmail.com>
2024-07-07 17:32:36 +1000

    Adding DOM manipulation seleting with id and class

--- first.js
+++ first.js
@@ -1,5 +1,13 @@
 //window.console.log("I am Samip Neapl");
 //alert("I am back");
 //console.log(window);
-console.dir(document.body);
-console.log(document.body);
+//console.dir(document.body);
+//console.log(document.body);
+//console.dir(document.head);
+let heading=document.getElementById("Samip")
+console.log(heading);//for print of html 
+console.dir(heading);
+let headings=document.getElementsByClassName("class")
+console.log(headings);//for print of html 
+console.dir(headings);
+
