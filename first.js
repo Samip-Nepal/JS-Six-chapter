986bc42b4048204be1e9fc6875ee5d695094d9e0
Samip Nepal <samipnepal5@gmail.com>
2024-07-07 19:46:53 +1000

    Pratice qn 1 and 2 by Samip Nepal

--- first.js
+++ first.js
@@ -1,9 +1,9 @@
-//window.console.log("I am Samip Neapl");
-//alert("I am back");
-//console.log(window);
-//console.dir(document.body);
-//console.log(document.body);
-//console.dir(document.head);
+/*window.console.log("I am Samip Neapl");
+alert("I am back");
+console.log(window);
+console.dir(document.body);
+console.log(document.body);
+console.dir(document.head);*/
 /*let heading=document.getElementById("Samip")
 console.log(heading);//for print of html 
 console.dir(heading);
@@ -13,7 +13,7 @@ console.dir(headings);
 let pra=document.getElementsByTagName("p")
 console.log(pra);//for print of html 
 console.dir(pra);*/
-let element=document.querySelector(".class")
+/*let element=document.querySelector(".class")
 console.dir(element);
 let elements=document.querySelectorAll("p")
 console.dir(elements);
@@ -27,4 +27,17 @@ console.log(elementss.innerHTML);
 console.log(ele.innerText="Abcd");//chaning the value of inner text.
 console.log(elementss.innerHTML="<i>Samit</i>");//Chaning the inner code.
 let id=document.querySelector("p")
-console.log(id.textContent);
+console.log(id.textContent);*/
+//Pratice Qn 1
+/*let pratice=document.querySelector("h2")
+console.log(pratice.innerText);
+pratice.innerText=pratice.innerText+"  From Madan bhandari clz";*/
+//Pratice qn 2
+let divs=document.querySelectorAll(".box")
+console.log(divs[0]);
+console.log(divs[1]);
+console.log(divs[2]);
+for(let i=0;i<10;i++)
+{
+    console.log(document.innerText=`Value of ${i}`);
+}
